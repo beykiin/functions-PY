ca5e4f9b88aa85244bd5d46d4d71cca922a19c0a
Yasin Beken <yasinbeken16@gmail.com>
2024-03-03 15:01:51 +0300

    First Commit

---
+++ functionPY.py
@@ -0,0 +1,63 @@
+# def writeToTheScreen():
+#     print("None function")
+# writeToTheScreen()
+
+# def writeToTheScreen2():
+#     return "returning a value"
+# print(writeToTheScreen2())=print("returning a value")
+
+# def nameSurname(name,surname):
+#     print(f"Name: {name} Surname: {surname}")
+# nameSurname("Yasin","Beken")
+# nameSurname(name="Yasin",surname="Beken")
+
+# def nameSurname(name,surname):
+#     return f"Name: {name} Surname: {surname}"
+# print(nameSurname("Yasin","Beken"))
+
+# If the length of the input entered from the keyboard is odd, delete 'a' and 'e'; if it's even, delete 't' and 'p' and print the modified string again.
+
+# 1. Method (My Implementation)
+# value=input("Please enter a things: ")
+
+# def writeToTheScreen():
+#     newValue=""
+#     if len(value)%2==0:
+#         for i in value:
+#             if i=="p" or i=="P" or i=="t" or i=="T":
+#                 continue
+#             else:
+#                 newValue+=i
+#         print(newValue)
+
+#     else:
+#         for i in value:
+#             if i=="a" or i=="A" or i=="e" or i=="E":
+#                 continue
+#             else:
+#                 newValue+=i
+#         print(newValue)
+# writeToTheScreen()
+
+# def deleteLetter(sentence):
+#     result=""
+#     if len(sentence)%2==0:
+#         for i in sentence:
+#             if i=="p" or i=="P" or i=="t" or i=="T":
+#                 continue
+#             result+=i
+#     else:
+#         for i in sentence:
+#             if i=="a" or i=="A" or i=="e" or i=="E":
+#                 continue
+#             result+=i
+#     return result
+# print(deleteLetter(input("Write Something: ")))
+
+
+                
+
+    
+
+
+
